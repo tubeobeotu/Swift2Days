0ec9f0a2ee7c790824468147077b17bec2984769
nguyenvantu <tu@techmaster.vn>
2016-10-05 01:39:46 +0700

    //update

--- SwiftShortCourse/othersamples/Generics/GenericsDemo.playground/Contents.swift
+++ SwiftShortCourse/othersamples/Generics/GenericsDemo.playground/Contents.swift
@@ -1,33 +1,33 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-//Func Generics
+//Generic Functions
 /*
- func adderInt(_ x: Int, _ y: Int) -> Int
- {
- return x + y
- }
- func adderString(_ x: Double, _ y: Double) -> Double
- {
- return x + y
- }
- func adderGeneric<T: NumericType>(_ x: T, _ y: T) -> T
- {
- print(type(of: x))
- return x + y
- }
- protocol NumericType {
- static func +(lhs: Self, rhs: Self) -> Self
- static func -(lhs: Self, rhs: Self) -> Self
- static func *(lhs: Self, rhs: Self) -> Self
- static func /(lhs: Self, rhs: Self) -> Self
- static func %(lhs: Self, rhs: Self) -> Self
- }
- 
- extension Int: NumericType{}
- extension Double: NumericType{}
- print(adderGeneric(1, 2.0))
- */
+//Đề bài hãy viết hàm cộng 2 số Int, Double và in ra đúng kiểu
+    func adderInt(_ x: Int, _ y: Int) -> Int
+    {
+        return x + y
+    }
+    func adderDouble(_ x: Double, _ y: Double) -> Double
+    {
+        return x + y
+    }
+
+//Khi sử dụng generic thì không cần quan tâm đến kiểu viết 1 dùng cho tất cả
+    func adderGeneric<T: NumericType>(_ x: T, _ y: T) -> T
+    {
+        print(type(of: x))
+        return x + y
+    }
+    protocol NumericType {
+        static func +(lhs: Self, rhs: Self) -> Self
+    }
+
+    extension Int: NumericType{}
+    extension Double: NumericType{}
+    print(adderGeneric(1, 2))
+*/
+
 //Struct
 /*
 struct Queue<Element>{
@@ -51,71 +51,96 @@ var q = Queue(array: [])
 q.enqueue(newElement: 4)
 q.enqueue(newElement: 2)
 q.enqueue(newElement: "Nguyen Van Tu")
-//q.dequeue()
-//q.dequeue()
-//q.dequeue()
-//q.dequeue()
+q.dequeue()
+q.dequeue()
+q.dequeue()
+q.dequeue()
 */
 
 //Extension
 /*
- extension Queue
- {
- func printAllValues()
- {
- for element in self.elements
- {
- print(element)
- }
- }
- }
- q.printAllValues()
- */
+extension Queue
+{
+    func printAllValues()
+    {
+        for element in self.elements
+        {
+            print(element)
+        }
+    }
+}
+q.printAllValues()
+*/
 
 //Constraints
 //Tìm index trong 1 mảng bất kỳ
 /*
-func findIndex<T: Equatable>(of valueToFind: T) -> Int? {
-    for (index, value) in self.elements.enumerated() {
-        if let elementValue = value as? T
-        {
-            if valueToFind == elementValue
+struct Queue<Element>{
+    fileprivate var elements = [Element]()
+    init(array: [Element]) {
+        self.elements = array
+    }
+    mutating func enqueue(newElement: Element) {
+        elements.append(newElement)
+    }
+    
+    mutating func dequeue() -> Element? {
+        guard !elements.isEmpty else {
+            return nil
+        }
+        return elements.remove(at: 0)
+    }
+    func findIndex<T: Equatable>(of valueToFind: T) -> Int? {
+        for (index, value) in self.elements.enumerated() {
+            if let elementValue = value as? T
             {
-                return index
+                if valueToFind == elementValue
+                {
+                    return index
+                }
             }
         }
+        return nil
     }
-    return nil
 }
+var q = Queue(array: [])
+
+q.enqueue(newElement: 4)
+q.enqueue(newElement: 2)
+q.enqueue(newElement: "Nguyen Van Tu")
 q.findIndex(of: "Nguyen Van Tu")
- */
+*/
 
 //Associated Types
-//protocol Container {
-//    associatedtype ItemType
-//    mutating func append(_ item: ItemType)
-//    var count: Int { get }
-//    subscript(i: Int) -> ItemType { get }
-//}
+/*
+protocol Container {
+    associatedtype ItemType
+    mutating func append(_ item: ItemType)
+    var count: Int { get }
+    subscript(i: Int) -> ItemType { get }
+}
+*/
 
 //Where Clauses
-//func allItemsMatch<C1: Container, C2: Container>
-//    (_ someContainer: C1, _ anotherContainer: C2) -> Bool
-//    where C1.ItemType == C2.ItemType, C1.ItemType: Equatable {
-////         Check that both containers contain the same number of items.
-//                    if someContainer.count != anotherContainer.count {
-//                        return false
-//                    }
-//                    // Check each pair of items to see if they are equivalent.
-//                    for i in 0..<someContainer.count {
-//                        if someContainer[i] != anotherContainer[i] {
-//                            return false
-//                        }
-//                    }
-////         All items match, so return true.
-//        return true
-//}
-//extension Array: Container{}
-//let a = [1, 2, 3, 4]
-//let b = [1, 2, 3]
-//print(allItemsMatch(a, b))
+/*
+func allItemsMatch<C1: Container, C2: Container>
+    (_ someContainer: C1, _ anotherContainer: C2) -> Bool
+    where C1.ItemType == C2.ItemType, C1.ItemType: Equatable {
+//         Check that both containers contain the same number of items.
+                    if someContainer.count != anotherContainer.count {
+                        return false
+                    }
+                    // Check each pair of items to see if they are equivalent.
+                    for i in 0..<someContainer.count {
+                        if someContainer[i] != anotherContainer[i] {
+                            return false
+                        }
+                    }
+//         All items match, so return true.
+        return true
+}
+extension Array: Container{}
+let a = [1, 2, 3, 4]
+let b = [1, 2, 3]
+print(allItemsMatch(a, b))
+*/
