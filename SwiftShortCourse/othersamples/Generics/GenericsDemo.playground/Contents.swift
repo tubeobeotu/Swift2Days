938152f4fbad03704da609c19f134cad55ed4f6b
Tuuu <tu@techmaster.vn>
2016-10-06 12:29:13 +0700

    //update generics

--- SwiftShortCourse/othersamples/Generics/GenericsDemo.playground/Contents.swift
+++ SwiftShortCourse/othersamples/Generics/GenericsDemo.playground/Contents.swift
@@ -8,12 +8,17 @@ func adderInt(_ x: Int, _ y: Int) -> Int
 {
     return x + y
 }
+
 func adderDouble(_ x: Double, _ y: Double) -> Double
 {
     return x + y
 }
 
-//Khi sử dụng generic thì không cần quan tâm đến kiểu viết 1 dùng cho tất cả
+func adderString(_ x: String, _ y: String) -> String
+{
+    return x + y
+}
+//Khi sử dụng generic thì không cần quan tâm đến kiểu cụ thể là gì
 func adderGeneric<T: NumericType>(_ x: T, _ y: T) -> T
 {
     print(type(of: x))
@@ -21,6 +26,8 @@ func adderGeneric<T: NumericType>(_ x: T, _ y: T) -> T
 }
 protocol NumericType {
     static func +(lhs: Self, rhs: Self) -> Self
+    //Lam vi du chia 2 string
+//    static func /(lhs: Self, rhs: Self) -> Self
 }
 
 extension Int: NumericType{}
@@ -28,10 +35,10 @@ extension Double: NumericType{}
 print(adderGeneric(1, 2))
 */
 
-//Struct, Class
 /*
-
+//Struct, Class
 struct Queue<Element>{
+
     fileprivate var elements = [Element]()
     init(array: [Element]) {
         self.elements = array
@@ -49,36 +56,33 @@ struct Queue<Element>{
         return elements.remove(at: 0)
     }
 }
-var q = Queue(array: [])
 
+var q = Queue(array: [])
 q.enqueue(newElement: 4)
 q.enqueue(newElement: 2)
 q.enqueue(newElement: "Nguyen Van Tu")
-q.dequeue()
-q.dequeue()
-q.dequeue()
-q.dequeue()
+//q.dequeue()
+//q.dequeue()
+//q.dequeue()
+//q.dequeue()
 */
 
-//Extension
 /*
-//Bản chất vẫn là struct và queue nên có thể sử dụng extension
-extension Queue
+//Extension
+//Bản chất vẫn là struct và class nên có thể sử dụng extension
+protocol Print
 {
     func printAllValues()
-    {
-        for element in self.elements
-        {
-            print(element)
-        }
-    }
+}
+extension Queue: Print
+{
 }
 q.printAllValues()
 */
 
+/*
 //Constraints
 //Tìm index trong 1 mảng bất kỳ
-/*
 struct Queue<Element>{
     fileprivate var elements = [Element]()
     init(array: [Element]) {
@@ -96,7 +100,7 @@ struct Queue<Element>{
     }
     func findIndex<T: Equatable>(of valueToFind: T) -> Int? {
         for (index, value) in self.elements.enumerated() {
-            
+            //Liệu có sử dụng được guard trong bài này
 //            guard let elementValue = value as? T else{return nil}
 //            if valueToFind == elementValue
 //            {
@@ -114,8 +118,7 @@ struct Queue<Element>{
     }
 }
 
-var qInit = Queue(array: [1, 2, "3"])
-qInit.findIndex(of: 1)
+
 
 var q = Queue(array: [])
 q.enqueue(newElement: 4)
@@ -123,6 +126,7 @@ q.enqueue(newElement: 2)
 q.enqueue(newElement: "Nguyen Van Tu")
 q.findIndex(of: "Nguyen Van Tu")
 */
+
 
 //Associated Types
 /*
@@ -171,3 +175,33 @@ let a = [1, 2, 3, 4]
 let b = [1, 2, 3, 4]
 print(allItemsMatch(a, b))
 */
+
+
+//protocol NumericType {
+//    static func +(lhs: Self, rhs: Self) -> Self
+//    //Lam vi du chia 2 string
+//    //    static func /(lhs: Self, rhs: Self) -> Self
+//}
+//protocol IntP
+//{
+//    associatedtype ItemType
+//}
+//extension Int: IntP, NumericType{
+//    typealias ItemType = Int
+//}
+//extension Double: IntP
+//{
+//    typealias ItemType = Double
+//}
+//
+//func testClause<C1: IntP, C2: IntP>(_ c1: C1, _ c2: C2) -> Bool
+//where C2: NumericType
+//{
+//    print(C2.ItemType.self)
+//    return true
+//}
+//testClause(2, 2)
+
+
+
+
